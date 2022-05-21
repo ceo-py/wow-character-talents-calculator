dc4b14e883e58d117af6ccf61991fef4775e380e
ceo-py <sfmf@abv.bg>
2022-05-21 14:53:10 +0200

    var and list clearing

--- script-taletns.js
+++ script-taletns.js
@@ -3,7 +3,6 @@ function ResetCell(cell) {
     tier = Generate_Buttons_Options(0, 0);
     ss = SpreadsheetApp.getActiveSpreadsheet();
     mainSheet = ss.getSheetByName("Talents");
-    mainSheet.getRange(cell).clear();
     dropdown = mainSheet.getRange(cell);
     rule = SpreadsheetApp.newDataValidation().requireValueInList(tier).build();
     dropdown.setDataValidation(rule);
@@ -26,24 +25,19 @@ function CreateSpec(cell, spend_points, max_points) {
     dropdown.setValue(spend_points);
 }
 
-function CreateCell(cell, tier_point_max, poinst_left) {
+function CreateCell(cell, tier_point_max, poinst_left, cell_value) {
     var cell_value, tier, ss, mainSheet, dropdown, rule;
-    cell_value = GetCellValue(cell);
-    if (cell_value === 0) {
-        tier = Generate_Buttons_Options(0, tier_point_max);
-        ss = SpreadsheetApp.getActiveSpreadsheet();
-        mainSheet = ss.getSheetByName("Talents");
-        dropdown = mainSheet.getRange(cell);
-        rule = SpreadsheetApp.newDataValidation()
-            .requireValueInList(tier)
-            .build();
-        dropdown.setDataValidation(rule);
-        dropdown.setBackground("#fcd299");
-        dropdown.setValue(cell_value);
-    }
+    tier = Generate_Buttons_Options(0, tier_point_max);
+    ss = SpreadsheetApp.getActiveSpreadsheet();
+    mainSheet = ss.getSheetByName("Talents");
+    dropdown = mainSheet.getRange(cell);
+    rule = SpreadsheetApp.newDataValidation()
+        .requireValueInList(tier)
+        .build();
+    dropdown.setDataValidation(rule);
+    dropdown.setBackground("#fcd299");
 }
 
-// 735 'AA10' 856 'AA11'
 function ButtonReset(cell, sheet) {
     var checkbox = SpreadsheetApp.getActive().getRange(sheet + cell);
     checkbox.uncheck();
@@ -86,94 +80,48 @@ function ResetTickButtons() {
     };
 }
 
-function GenerateSpec(type) {
-    if (type === 735) {
-        spec_735 = {
-            G7: [0, 5],
-            I7: [5, 5],
-            E11: [3, 3],
-            G11: [2, 2],
-            K11: [0, 2],
-            E15: [1, 3],
-            G15: [3, 3],
-            I15: [1, 1],
-            K15: [0, 2],
-            G19: [0, 5],
-            I19: [3, 3],
-            Q7: [2, 2],
-            S7: [3, 3],
-            U7: [0, 5],
-            Q11: [0, 5],
-            S11: [2, 3],
-            U11: [3, 3],
-            Q15: [3, 3],
-            S15: [1, 1],
-            U15: [2, 2],
-            S19: [1, 5],
-            U19: [3, 3],
-            Q23: [0, 1],
-            S23: [5, 5],
-            W23: [0, 2],
-            Q27: [2, 2],
-            U27: [5, 5],
-            Q31: [2, 3],
-            S31: [1, 1],
-            U31: [0, 3],
-            S35: [5, 5],
-            U35: [0, 3],
-            Q39: [3, 3],
-            S39: [1, 1],
-            U39: [3, 3],
-            Q43: [0, 2],
-            U43: [5, 5],
-            S47: [1, 1],
-        };
-        return spec_735;
-    }
-    if (type === 856) {
-        spec_856 = {
-            G7: [0, 5],
-            I7: [5, 5],
-            E11: [3, 3],
-            G11: [2, 2],
-            K11: [0, 2],
-            E15: [0, 3],
-            G15: [0, 3],
-            I15: [1, 1],
-            K15: [0, 2],
-            G19: [0, 5],
-            I19: [0, 3],
-            Q7: [2, 2],
-            S7: [3, 3],
-            U7: [0, 5],
-            Q11: [0, 5],
-            S11: [2, 3],
-            U11: [3, 3],
-            Q15: [3, 3],
-            S15: [1, 1],
-            U15: [2, 2],
-            S19: [1, 5],
-            U19: [3, 3],
-            Q23: [1, 1],
-            S23: [5, 5],
-            W23: [0, 2],
-            Q27: [2, 2],
-            U27: [5, 5],
-            Q31: [3, 3],
-            S31: [1, 1],
-            U31: [2, 3],
-            S35: [5, 5],
-            U35: [3, 3],
-            Q39: [3, 3],
-            S39: [1, 1],
-            U39: [3, 3],
-            Q43: [0, 2],
-            U43: [5, 5],
-            S47: [1, 1],
-        };
-
-        return spec_856;
-    }
+function GenerateSpec() {
+    spec = {
+        G7: [0, 0, 5],
+        I7: [5, 5, 5],
+        E11: [3, 3, 3],
+        G11: [2, 2, 2],
+        K11: [0, 0, 2],
+        E15: [1, 0, 3],
+        G15: [3, 0, 3],
+        I15: [1, 1, 1],
+        K15: [0, 0, 2],
+        G19: [0, 0, 5],
+        I19: [3, 0, 3],
+        Q7: [2, 2, 2],
+        S7: [3, 3, 3],
+        U7: [0, 0, 5],
+        Q11: [0, 0, 5],
+        S11: [2, 2, 3],
+        U11: [3, 3, 3],
+        Q15: [3, 3, 3],
+        S15: [1, 1, 1],
+        U15: [2, 2, 2],
+        S19: [1, 1, 5],
+        U19: [3, 3, 3],
+        Q23: [0, 1, 1],
+        S23: [5, 5, 5],
+        W23: [0, 0, 2],
+        Q27: [2, 2, 2],
+        U27: [5, 5, 5],
+        Q31: [2, 3, 3],
+        S31: [1, 1, 1],
+        U31: [0, 2, 3],
+        S35: [5, 5, 5],
+        U35: [0, 3, 3],
+        Q39: [3, 3, 3],
+        S39: [1, 1, 1],
+        U39: [3, 3, 3],
+        Q43: [0, 0, 2],
+        U43: [5, 5, 5],
+        S47: [1, 1, 1],
+    };
+    return spec;
 }
 
 function onEdit(e) {
@@ -181,34 +129,9 @@ function onEdit(e) {
         sheetName,
         cell,
         a1,
-        tier_one_and_two_result,
         result_tier4,
         poinst_left_to_spend,
-        starlight,
-        genesis,
-        moonglow,
-        nature_majesty,
-        improved_moonfire,
-        naturalice,
-        subtlety,
-        nature_shape_shifter,
-        intensity,
-        master_shape_shifte,
-        imp_rej,
-        nature_switfness,
-        gift_of_nature,
-        emp_touch,
-        swiftmend,
-        emp_rej,
-        revitalize,
         tree_of_life,
-        imp_tree_of_life,
-        wild_growth,
-        mark_of_wild,
-        nature_focus,
-        furor,
-        tier_first_row,
-        tier_second_row,
         tier_three_row_result,
         tier_four_row_result,
         tier_five_row_result,
@@ -230,73 +153,12 @@ function onEdit(e) {
     // Browser.msgBox(mainSheet.getRange(column, row).getValue())
     // var aaa = column + row
     // console.log(aaa)
-    const buttonscells = [
-        "G7",
-        "I7",
-        "E11",
-        "G11",
-        "K11",
-        "E15",
-        "G15",
-        "I15",
-        "K15",
-        "G19",
-        "I19",
-    ];
-    const buttons_resto = [
-        "Q11",
-        "S11",
-        "U11",
-        "Q15",
-        "S15",
-        "U15",
-        "S19",
-        "U19",
-        "Q23",
-        "S23",
-        "W23",
-        "Q27",
-        "U27",
-        "Q31",
-        "S31",
-        "U31",
-        "S35",
-        "U35",
-        "Q39",
-        "S39",
-        "U39",
-        "Q43",
-        "U43",
-        "S47",
-        "Q7",
-        "S7",
-        "U7",
-    ];
-    const buttons_four_peace = ["T5", "T7", "T9", "T11"];
-    const buttons_two_peace = ["T4", "T6", "T8", "T10"];
 
-    if (sheetName === "Talents" && buttonscells.includes(a1)) {
+    if (sheetName === "Talents" && Object.keys(GenerateSpec()).slice(0, 11).includes(a1)) {
         ButtonReset("AA11", "Talents!");
         ButtonReset("AA10", "Talents!");
-        poinst_left_to_spend = GetCellValue("AC2");
-        // CreateCell('G7', 5, poinst_left_to_spend);
-        // CreateCell('I7', 5, poinst_left_to_spend);
-        starlight = GetCellValue("G7");
-        genesis = GetCellValue("I7");
-        moonglow = GetCellValue("E11");
-        nature_majesty = GetCellValue("G11");
-        improved_moonfire = GetCellValue("K11");
-        // var brambles = GetCellValue('E15');
-        // var nature_grace = GetCellValue('G15');
-        // var nature_spelndor = GetCellValue('I15');
-        // var nature_reach = GetCellValue('K15');
-        // var vengeance = GetCellValue('G19');
-        // var celestial_focus = GetCellValue('I19');
-        tier_one_and_two_result =
-            starlight + genesis + moonglow + nature_majesty + improved_moonfire;
-        // var test123 = GetCellValue('B18');
-        if (starlight + genesis < 5) {
-            buttonscells.forEach((cell) => {
+        if (GetCellValue("G7") + GetCellValue("I7") < 5) {
+            Object.keys(GenerateSpec()).slice(0, 11).forEach((cell) => {
                 if (cell !== "I7" && cell !== "G7") {
                     ResetCell(cell);
                 }
@@ -305,8 +167,7 @@ function onEdit(e) {
             CreateCell("E11", 3, poinst_left_to_spend);
             CreateCell("G11", 2, poinst_left_to_spend);
             CreateCell("K11", 2, poinst_left_to_spend);
-
-            if (tier_one_and_two_result < 10) {
+            if (GetCellValue("B14") != 0) {
                 ResetCell("E15");
                 ResetCell("K15");
                 ResetCell("G15");
@@ -314,11 +175,11 @@ function onEdit(e) {
             } else {
                 CreateCell("E15", 3, poinst_left_to_spend);
                 CreateCell("K15", 2, poinst_left_to_spend);
-                if (nature_majesty === 2) {
+                if (GetCellValue("G11") === 2) {
                     CreateCell("G15", 3, poinst_left_to_spend);
                     CreateCell("I15", 1, poinst_left_to_spend);
                 }
-                if (nature_majesty !== 2) {
+                else {
                     ResetCell("G15");
                     ResetCell("I15");
                 }
@@ -333,60 +194,23 @@ function onEdit(e) {
             }
         }
     }
-    if (sheetName === "Talents" && buttons_resto.includes(a1)) {
+    if (sheetName === "Talents" && Object.keys(GenerateSpec()).slice(11).includes(a1)) {
         ButtonReset("AA11", "Talents!");
         ButtonReset("AA10", "Talents!");
-        naturalice = GetCellValue("Q11");
-        subtlety = GetCellValue("S11");
-        nature_shape_shifter = GetCellValue("U11");
-        intensity = GetCellValue("Q15");
-        // var clear_casting = GetCellValue('S15');
-        master_shape_shifte = GetCellValue("U15");
-        // var tranquil_spirit = GetCellValue('S19');
-        imp_rej = GetCellValue("U19");
-        nature_switfness = GetCellValue("Q23");
-        gift_of_nature = GetCellValue("S23");
-        // var imp_tranq = GetCellValue('W23');
-        emp_touch = GetCellValue("Q27");
-        // var natures_bounty = GetCellValue('U27');
-        // var living_spirit = GetCellValue('Q31');
-        swiftmend = GetCellValue("S31");
-        // var nature_perfection = GetCellValue('U31');
-        emp_rej = GetCellValue("S35");
-        // var living_seed = GetCellValue('U35');
-        revitalize = GetCellValue("Q39");
-        tree_of_life = GetCellValue("S39");
-        imp_tree_of_life = GetCellValue("U39");
-        // var imp_barkskin = GetCellValue('Q43');
-        // var gote = GetCellValue('U43');
-        wild_growth = GetCellValue("S47");
-        mark_of_wild = GetCellValue("Q7");
-        nature_focus = GetCellValue("S7");
-        furor = GetCellValue("U7");
-        tier_first_row = mark_of_wild + nature_focus + furor;
-        tier_second_row =
-            tier_first_row + naturalice + subtlety + nature_shape_shifter;
-        // var tier_three_row = tier_second_row + intensity + clear_casting + master_shape_shifte
-        // var tier_four_row = tier_three_row + tranquil_spirit + imp_rej
-        // var tier_five_row = tier_four_row + gift_of_nature + nature_switfness + imp_tranq
-        // var tier_six_row = tier_five_row + emp_touch + natures_bounty
-        // var tier_seven_row = tier_six_row + living_spirit + swiftmend + nature_perfection
-        // var tier_eigth_row = tier_seven_row + emp_rej + living_seed
-        // var tier_nine_row = tier_eigth_row + revitalize + tree_of_life + imp_tree_of_life
-        // var tier_ten_row = tier_nine_row + imp_barkskin + gote
-        if (tier_first_row < 5) {
-            buttons_resto.forEach(function (cell) {
+        N10 = GetCellValue("N10")
+        if (N10 != 0) {
+            Object.keys(GenerateSpec()).slice(11).forEach(function (cell) {
                 if (cell !== "Q7" && cell !== "S7" && cell !== "U7") {
                     ResetCell(cell);
                 }
             });
         } else {
-            if (tier_first_row > 4) {
+            if (N10 == 0) {
                 CreateCell("Q11", 5, poinst_left_to_spend);
                 CreateCell("S11", 3, poinst_left_to_spend);
                 CreateCell("U11", 3, poinst_left_to_spend);
             }
-            if (tier_second_row < 10) {
+            if (GetCellValue("N14") != 0) {
                 ResetCell("Q15");
                 ResetCell("S15");
                 ResetCell("U15");
@@ -394,9 +218,9 @@ function onEdit(e) {
                 CreateCell("Q15", 3, poinst_left_to_spend);
                 CreateCell("S15", 1, poinst_left_to_spend);
 
-                if (nature_shape_shifter !== 3) {
+                if (GetCellValue("U11") !== 3) {
                     ResetCell("U15");
-                } else if (master_shape_shifte === 0) {
+                } else {
                     CreateCell("U15", 2, poinst_left_to_spend);
                 }
             }
@@ -417,9 +241,9 @@ function onEdit(e) {
                 CreateCell("S23", 5, poinst_left_to_spend);
                 CreateCell("W23", 2, poinst_left_to_spend);
 
-                if (intensity !== 3) {
+                if (GetCellValue("Q15") !== 3) {
                     ResetCell("Q23");
-                } else if (nature_switfness === 0) {
+                } else if (GetCellValue("Q23") === 0) {
                     CreateCell("Q23", 1, poinst_left_to_spend);
                 }
             }
@@ -427,11 +251,9 @@ function onEdit(e) {
             if (tier_five_row_result !== 0) {
                 ResetCell("Q27");
                 ResetCell("U27");
-            } else if (emp_touch === 0) {
+            } else
                 CreateCell("Q27", 2, poinst_left_to_spend);
-            }
-
-            if (imp_rej === 3 && tier_five_row_result === 0) {
+            if (GetCellValue("U19") === 3 && tier_five_row_result === 0) {
                 CreateCell("U27", 5, poinst_left_to_spend);
             } else {
                 ResetCell("U27");
@@ -445,11 +267,11 @@ function onEdit(e) {
                 CreateCell("Q31", 3, poinst_left_to_spend);
                 CreateCell("U31", 3, poinst_left_to_spend);
 
-                if (gift_of_nature !== 5) {
+                if (GetCellValue("S23") !== 5) {
                     ResetCell("S31");
-                } else if (swiftmend === 0) {
+                } else
                     CreateCell("S31", 1, poinst_left_to_spend);
-                }
+
             }
             tier_seven_row_result = GetCellValue("N34");
             if (tier_seven_row_result !== 0) {
@@ -464,11 +286,10 @@ function onEdit(e) {
                 ResetCell("Q39");
                 ResetCell("S39");
                 ResetCell("U39");
-            } else if (revitalize === 0) {
+            } else {
                 CreateCell("Q39", 3, poinst_left_to_spend);
             }
-
-            if (emp_rej !== 5) {
+            if (GetCellValue("S35") !== 5) {
                 ResetCell("S39");
                 ResetCell("S47");
                 ResetCell("U39");
@@ -476,7 +297,7 @@ function onEdit(e) {
                 CreateCell("S39", 1, poinst_left_to_spend);
             }
             tier_nine_row_result = GetCellValue("N42");
-            if (tier_nine_row_result != 0) {
+            if (tier_nine_row_result !== 0) {
                 ResetCell("Q43");
                 ResetCell("U43");
             } else {
@@ -484,14 +305,15 @@ function onEdit(e) {
                 CreateCell("U43", 5, poinst_left_to_spend);
             }
             tier_ten_row_result = GetCellValue("N46");
+            tree_of_life = GetCellValue("S39")
             if (tier_ten_row_result !== 0) {
                 ResetCell("S47");
             } else {
-                if (tree_of_life === 1 && wild_growth === 0) {
+                if (tree_of_life === 1) {
                     CreateCell("S47", 1, poinst_left_to_spend);
                 }
             }
-            if (tree_of_life === 1 && imp_tree_of_life === 0) {
+            if (tree_of_life === 1) {
                 CreateCell("U39", 3, poinst_left_to_spend);
             }
             if (tree_of_life !== 1) {
@@ -503,14 +325,14 @@ function onEdit(e) {
     cell = SpreadsheetApp.getActiveSheet().getActiveCell();
     check_box_value = cell.getValue();
     if (sheetName === "Talents" && a1 === "AA10" && check_box_value === true) {
-        for (const [key, value] of Object.entries(GenerateSpec(735))) {
-            CreateSpec(key, value[0], value[1]);
+        for (const [key, value] of Object.entries(GenerateSpec())) {
+            CreateSpec(key, value[0], value[2]);
         }
         ButtonReset("AA11", "Talents!");
     }
     if (sheetName === "Talents" && a1 === "AA11" && check_box_value === true) {
-        for (const [key, value] of Object.entries(GenerateSpec(856))) {
-            CreateSpec(key, value[0], value[1]);
+        for (const [key, value] of Object.entries(GenerateSpec())) {
+            CreateSpec(key, value[1], value[2]);
         }
         ButtonReset("AA10", "Talents!");
         ResetCell("G19");
@@ -518,24 +340,24 @@ function onEdit(e) {
     }
     if (
         sheetName === "Calculator" &&
-        buttons_four_peace.includes(a1) &&
+        Object.keys(ResetTickButtons()).filter((_, i) => i % 2 === 1).includes(a1) &&
         check_box_value === true
     ) {
         AutoTickFourBonus(a1, "Calculator!");
     }
 
-    if (sheetName === "Calculator" && buttons_two_peace.includes(a1)) {
-        buttons_four_peace.forEach(function (cell) {
+    if (sheetName === "Calculator" && Object.keys(ResetTickButtons()).filter((_, i) => i % 2 === 0).includes(a1)) {
+        Object.keys(ResetTickButtons()).filter((_, i) => i % 2 === 1).forEach(function (cell) {
             ButtonReset(cell, "Calculator!");
         });
         buttons_check_number = 0;
-        buttons_two_peace.forEach((cell) => {
+        Object.keys(ResetTickButtons()).filter((_, i) => i % 2 === 0).forEach((cell) => {
             if (GetCellValue(cell)) {
                 buttons_check_number++;
             }
         });
         if (buttons_check_number > 2) {
-            buttons_two_peace.forEach((cell) => {
+            Object.keys(ResetTickButtons()).filter((_, i) => i % 2 === 0).forEach((cell) => {
                 if (cell !== a1) {
                     ButtonReset(cell, "Calculator!");
                 }
---
+++ test.js
@@ -0,0 +1,13 @@
+var test_ = {
+    d1: '1',
+    d2: '2',
+    d3: '3',
+    d4: '4',
+    d5: '5',
+    d6: '6'
+};
+
+var showMe = Object.keys(test_).slice(0,2);
+if(Object.keys(test_).slice(0,).includes('d1')){
+    console.log(Object.keys(test_).slice(2));
+}
